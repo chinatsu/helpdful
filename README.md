cf684dd2d31bd5fd983759f8f1e3ec55b7b38656
Kent Stefan Daleng <kent.stefan.daleng@nav.no>
2018-07-16 14:45:50 +0200

    Code formatting, updates README with installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # helpdful
 pdf generating thing!
+
+## Installation
+
+For development purposes, it is recommended to use a virtual environment
+to keep the project isolated.
+
+1. `git clone` this repository, and enter its directory
+2. Run `python -m venv .` to initiate a virtual environment
+3. Activate the virtual environment,
+  * `Scripts\activate.bat` for Windows, cmd.exe
+  * `Scripts\activate.ps1` for Windows, PowerShell
+  * `source bin/activate` for Bash/Zsh
+  * `. bin/activate.fish` for Fish
+  * `source bin/activate.csh` for Csh/Tcsh
+4. Run `pip install -e .` to install the module and its dependencies
+5. Run `python -m helpdful` to run the script and generate a file
+named `test.pdf`
+
+### Recommendations
+
+It is recommended to use [Black](https://github.com/ambv/black) for code
+formatting before pushing changes. Simply run `pip install black` and then
+run `black helpdful` to have it format your changes.
--- helpdful/test_data.py
+++ helpdful/test_data.py
@@ -4,55 +4,39 @@ data = {
     "title": "Søknad om å beholde sykepenger utenfor Norge",
     "date": datetime(2017, 2, 1),
     "application_id": "3c87f6e2-66ee-4376-9d4d-2444dea8368e",
-    "person": {
-        "id": 12068745698,
-        "name": "Kari Normann Paulsrud Granholt"
-    },
+    "person": {"id": 12068745698, "name": "Kari Normann Paulsrud Granholt"},
     "questions": [
         {
             "text": "Når skal du oppholde deg utenfor Norge?",
             "type": "PERIODER",
-            "answer": "01.01.2017 – 31.01.2017"
+            "answer": "01.01.2017 – 31.01.2017",
         },
         {
             "text": "Hvilket land skal du reise til?",
             "type": "LAND",
-            "answer": "Tyskland"
+            "answer": "Tyskland",
         },
-        {
-            "text": "Har du arbeidsgiver?",
-            "type": "CHECKBOKS",
-            "answer": "Ja"
-        },
-        {
-            "text": "Er du 100% sykmeldt?",
-            "type": "CHECKBOKS",
-            "answer": "Ja"
-        },
+        {"text": "Har du arbeidsgiver?", "type": "CHECKBOKS", "answer": "Ja"},
+        {"text": "Er du 100% sykmeldt?", "type": "CHECKBOKS", "answer": "Ja"},
         {
             "text": "Før du reiser må du bekrefte at",
             "information": [
                 "Oppholdet utenfor Norge ikke medfører at helsetilstanden din blir forverret",
                 "Oppholdet utenfor Norge ikke vil forlenge arbeidsuførheten din eller hindre planlagt behandling",
-                "Du har avklart dette med sykmelderen din"
+                "Du har avklart dette med sykmelderen din",
             ],
             "type": "IKKE_RELEVANT",
-            "answer": "Jeg bekrefter at jeg har gjort med kjent med pliktene mine"
-        }
+            "answer": "Jeg bekrefter at jeg har gjort med kjent med pliktene mine",
+        },
     ],
 }
 
 theme = {
-    "footer": {
-        "height": 20,
-    },
+    "footer": {"height": 20},
     "page_margin": 40,
     "header": {
         "background": (0.2146, 0.0526, 0.0, 0.0314),
-        "logo": {
-            "scale": 0.8,
-            "margin_right": 4,
-        },
+        "logo": {"scale": 0.8, "margin_right": 4},
         "color": (0, 0, 0, 1),
         "font_size": 16,
         "font_weight": "700",
@@ -60,23 +44,14 @@ theme = {
         "margin_bottom": 40,
     },
     "information": {
-        "name": {
-            "font_size": 14,
-            "font_weight": "700",
-        },
-        "id": {
-            "font_size": 12,
-            "font_weight": "regular",
-        },
-        "date": {
-            "font_size": 8,
-            "font_weight": "regular",
-        }
+        "name": {"font_size": 14, "font_weight": "700"},
+        "id": {"font_size": 12, "font_weight": "regular"},
+        "date": {"font_size": 8, "font_weight": "regular"},
     },
     "icon_margin": 10,
     "images": {
         "logo": "resources/Navlogo.svg",
         "person": "resources/Personikon.svg",
         "checkboks": "resources/Checkboks.svg",
-    }
+    },
 }
